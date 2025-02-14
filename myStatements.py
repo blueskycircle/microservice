5cfb68ee7e34168c26d43de2093c58652d653235
blueskycircle <gngerogiannis@gmail.com>
2025-02-14 23:17:52 +0000

    fix formatting make format

--- myStatements.py
+++ myStatements.py
@@ -4,5 +4,5 @@ my_dict = {"drink": "coffee", "milk": "whole"}
 for item in my_list:
     print(f"My favourite thing to eat is: {item}")
 
-for _,value in my_dict.items():
+for _, value in my_dict.items():
     print(f"My favourite thing to drink is: {value}")
